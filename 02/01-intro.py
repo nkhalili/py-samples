6b9ed8df8317100b4c219f9d597af1b413fad041
Navid Khalili <nkhalili@live.com>
2020-07-12 14:41:44 -0400

    string interpolation

---
+++ 02/01-intro.py
@@ -0,0 +1,8 @@
+import platform
+
+
+def main():
+    print("This is python version {}".format(platform.python_version()))
+
+if __name__ == "__main__":
+    main()
---
+++ 02/02-string-interpolation.py
@@ -0,0 +1,10 @@
+
+# python 3, uses string as a object
+x = 3
+print("this is number {}".format(x))
+
+# python 3.6+
+print(f"this is number {x}")
+
+# python 2+, %d for Integer
+print("This is number %d" % x)
